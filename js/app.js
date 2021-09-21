8fff8d419eccd4f60789cc52dcd8993fce133199
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-21 14:48:25 +0200

    adding bubbles to every section while scrolling

--- js/app.js
+++ js/app.js
@@ -17,13 +17,15 @@
  * Define Global Variables
  * 
 */
-let navBarList = document.getElementById('navbar__list');
-let mySections = document.querySelectorAll("section");
+let navBarList = document.getElementById('navbar__list');  // The ul tag
+let mySections = document.querySelectorAll("section");   // All Sections
+let goUpBtn = document.querySelector(".go-up");   // Go Up BTN
+
 
 /**
  * Begin Main Functions
  * 
-*/
+    */
 
 // =============== build the nav ===============
 (function buildNav() {
@@ -42,8 +44,6 @@ let mySections = document.querySelectorAll("section");
 // ==================================================================================
 
 // =============== Show a BTN after a specific scroll to let the user go up ===============
-let goUpBtn = document.querySelector(".go-up");
-
 window.addEventListener("scroll", ()=> {
     if (window.scrollY > 300) {
         goUpBtn.style.opacity = "1";
@@ -53,6 +53,43 @@ window.addEventListener("scroll", ()=> {
     }
 });
 
+// ==================================================================================
+// =============== Section Highlighted via bubble ===============
+let sec1 = document.getElementById("section1");
+let sec2 = document.getElementById("section2");
+let sec3 = document.getElementById("section3");
+let sec4 = document.getElementById("section4");
+let sec5 = document.getElementById("section5");
+let sec6 = document.getElementById("section6");
+let sec7 = document.getElementById("section7");
 
-
+window.addEventListener("scroll", ()=> {
+    if (window.scrollY > 300 && window.scrollY < 700 ) {
+        console.log("Section one is here");
+    }
+    else if (window.scrollY > 860 && window.scrollY < 1280) {
+        sec1.classList.remove("your-active-class");
+        sec2.classList.add("your-active-class");
+    }
+    else if (window.scrollY > 1420 && window.scrollY < 1950) {
+        sec2.classList.remove("your-active-class");
+        sec3.classList.add("your-active-class");
+    }
+    else if (window.scrollY > 2040 && window.scrollY < 2450) {
+        sec3.classList.remove("your-active-class");
+        sec4.classList.add("your-active-class");
+    }
+    else if (window.scrollY > 2600 && window.scrollY < 3100) {
+        sec4.classList.remove("your-active-class");
+        sec5.classList.add("your-active-class");
+    }
+    else if (window.scrollY > 3240 && window.scrollY < 3660) {
+        sec5.classList.remove("your-active-class");
+        sec6.classList.add("your-active-class");
+    }
+    else if (window.scrollY > 3770 && window.scrollY < 4100) {
+        sec6.classList.remove("your-active-class");
+        sec7.classList.add("your-active-class");
+    }
+});
 
